8807de73ee1cdfb9edacfe48dbc5de27bac15224
Sealshusky <129021061+Sealshusky@users.noreply.github.com>
2025-10-15 17:25:26 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,51 @@
+console.log(add(10, 2));
+
+function add(a, b) {
+  return a + b;
+};
+
+const subtract = function (c, d) {
+  return c - d;
+};
+
+console.log(subtract(10, 2));
+
+const multiply = (e, f) => e * f;
+
+console.log(multiply(10, 2));
+
+function divide(g, h) {
+  if (h === 0) {
+    return "Cannot divide by zero";
+  }
+  return g / h;
+};
+
+console.log(divide(10,2));
+
+function calculate(a, b, operation) {
+  if (operation === "+") {
+    return add(a,b);
+  }
+  else if (operation === "-") {
+    return subtract(a,b);
+  }
+  else if (operation === "*") {
+    return multiply(a,b);
+  }
+  else if (operation === "/") {
+    return divide(a,b);
+  }
+  else {
+    return "Unknown operator";
+  };
+};
+
+console.log(calculate(10,2,"+"));
+console.log(calculate(10,2,"-"));
+console.log(calculate(10,2,"*"));
+console.log(calculate(10,2,"/"));
+    
+    
+
 
